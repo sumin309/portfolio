a55f0973023d3d121c5d2d345869aded6ad408e1
sumin309 <smhong309@gmail.com>
2023-12-15 21:12:30 +0900

    4

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,26 @@
-// tailwind.config.js
-const defaultTheme = require("tailwindcss/defaultTheme");
-
+/** @type {import('tailwindcss').Config} */
 module.exports = {
+  content: ["index.html"],
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
-      },
+    fontFamily: {
+      head: ["Poppins"],
+      body: ["Nunito Sans"],
     },
+    fontSize: {
+      cp: ["14px"],
+      bd3: ["16px"],
+      bd2: ["18px"],
+      bd1: ["20px"],
+      hd3: ["24px"],
+      hd2: ["28px"],
+      hd1: ["45px"],
+    },
+    screens: {
+      sm: "576px",
+      md: "960px",
+      lg: "1440px",
+    },
+    extend: {},
   },
-  // ...
+  plugins: [],
 };
